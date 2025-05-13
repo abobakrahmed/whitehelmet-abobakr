7fffb981aa4f1abcb4957b6a9c99b1422b37a05a
root <root@Abobakr-PC>
2025-05-13 18:59:17 +0300

    add whitelement files

---
+++ terraform/argocd.tf
@@ -0,0 +1,34 @@
+data "google_client_config" "default" {}
+
+provider "kubernetes" {
+  host                   = google_container_cluster.primary.endpoint
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = google_container_cluster.primary.endpoint
+    token                  = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
+  }
+}
+
+resource "helm_release" "argocd" {
+  name       = "argocd"
+  namespace  = "argocd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  version    = "5.51.6" # check for latest if needed
+
+  create_namespace = true
+
+  values = [
+    <<EOF
+server:
+  service:
+    type: LoadBalancer
+EOF
+  ]
+}
+
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,6 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+project_id = "abiding-casing-457600-s0"
+region     = "us-east1"
+zone       = "us-east1-b"
